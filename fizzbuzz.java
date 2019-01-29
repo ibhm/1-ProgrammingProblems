f59fc2e39d01161f3529324e87f3a3248252853c
Malik Ibrahim <ibhm@live.com>
2019-01-28 23:13:46 -0500

    fizzbuzz

---
+++ fizzbuzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> array = new ArrayList<>();
+        
+        for(int i = 1; i <= n ;i++){
+            if(i%5 == 0 && i%3 ==0 ){
+                array.add("FizzBuzz");
+            }
+            else if(i%3 == 0 ){
+                array.add("Fizz");
+            }
+            else if (i%5 == 0 ){
+                array.add("Buzz");
+            }
+            else{
+                array.add(Integer.toString(i));}
+        }
+        return array;
+    }
+
+}
