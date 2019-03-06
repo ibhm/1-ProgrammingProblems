4415d15792542630780ff7fcc47418a58316cf21
Malik Ibrahim <ibhm@live.com>
2019-03-06 11:06:48 -0500

    A

--- TwoSum.java
+++ TwoSum.java
@@ -2,14 +2,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                int check = target - nums[i];
-                if (nums[j]==check) {
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    //return 2 items using a list
                     return new int[] {i, j};
                 }
             }
-        
-        }  
-        throw new IllegalArgumentException("None found");
+        }
+    throw new IllegalArgumentException("None"); 
     }
 }
