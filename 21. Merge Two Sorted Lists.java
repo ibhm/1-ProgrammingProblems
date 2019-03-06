c80eb915e56ba0fd6222e90b4420b2fc22f5b673
Malik Ibrahim <ibhm@live.com>
2019-03-06 10:39:36 -0500

    21.mod

--- 21. Merge Two Sorted Lists.java
+++ 21. Merge Two Sorted Lists.java
@@ -14,6 +14,9 @@ class Solution {
         if (l2 == null){
             return l1;
         }
+
+        //now we check the first values of each list
+        //start with lower value. the NEXT value after that, is either the first value of the other list OR the second value in the first list
         else if(l1.val < l2. val){
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
