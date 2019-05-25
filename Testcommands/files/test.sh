eda5be95392e29853a920ef4517bc663d5fc5a0b
Malik Ibrahim <maliki2@mcmaster.ca>
2019-05-25 15:56:27 -0400

    Update

---
+++ Testcommands/files/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv /Users/ibrahimmalik/1-Projects/1-ProgrammingProblems/Testcommands/old/* /Users/ibrahimmalik/1-Projects/1-ProgrammingProblems/Testcommands/files
+
---
+++ Testcommands/old/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /Users/ibrahimmalik/1-Projects/1-ProgrammingProblems/Testcommands/test.sh /Users/ibrahimmalik/1-Projects/1-ProgrammingProblems/Testcommands/old/
+
